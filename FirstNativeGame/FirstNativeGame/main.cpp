769edd2cce69832e356ffefc38af98d0ddb1ab44
koganei <khoury.marc@gmail.com>
2013-12-04 20:11:08 -0500

    Initial Commit
    Just the loop.

---
+++ FirstNativeGame/FirstNativeGame/main.cpp
@@ -0,0 +1,17 @@
+// My First Native Game!
+
+int main(void) {
+	bool done = false;
+	
+	int i = 0;
+	while (!done) {
+		// main loop!
+		i++;
+
+		if (i == 5000000000000) {
+			done = true;
+		}
+		
+	}
+	return 0;
+}
